feee5c8084b0e4f6dcfc98fca7df31d283d91d73
Haoz <developerhaon@gmail.com>
2017-11-24 00:06:15 +0800

    久违的省赛，好快啊，又一年了。

---
+++ app/src/main/java/com/developerhaoz/androidloaderdemo/AlbumLoader.java
@@ -0,0 +1,77 @@
+package com.developerhaoz.androidloaderdemo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+
+/**
+ * @author Haoz
+ * @date 2017/11/22.
+ */
+
+public class AlbumLoader extends CursorLoader {
+
+    public static final String COLUMN_COUNT = "count";
+
+    /**
+     * content://media/external/file
+     */
+    private static final Uri QUERY_URI = MediaStore.Files.getContentUri("external");
+
+    private static final String[] COLUMNS = {
+            MediaStore.Files.FileColumns._ID,
+            "bucket_id",
+            "bucket_display_name",
+            MediaStore.MediaColumns.DATA,
+            COLUMN_COUNT};
+
+    private static final String[] PROJECTION = {
+            MediaStore.Files.FileColumns._ID,
+            "bucket_id",
+            "bucekt_display_name",
+            MediaStore.MediaColumns.DATA,
+            "COUNT(*) AS " + COLUMN_COUNT};
+
+    /**
+     * (media_type=? OR media_type =?) AND _size>0) GROUP BY (bucket_id
+     */
+    private static final String SELECTION =
+            "(" + MediaStore.Files.FileColumns.MEDIA_TYPE + "=?"
+            + " OR "
+            + MediaStore.Files.FileColumns.MEDIA_TYPE + "=?)"
+            + " AND " + MediaStore.MediaColumns.SIZE + ">0"
+            + ") GROUP BY (bucket_id";
+
+    private static final String[] SELECTION_ARGS = {
+            String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE),
+            String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)
+    };
+
+    private static final String BUCKET_ORDER_BY = "datetaken DESC";
+
+    private AlbumLoader(Context context, String selection, String[] selectionArgs){
+        super(context, QUERY_URI, PROJECTION, SELECTION, SELECTION_ARGS, BUCKET_ORDER_BY);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
